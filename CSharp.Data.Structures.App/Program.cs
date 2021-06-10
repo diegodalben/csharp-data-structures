3221baa317f4d61c57fc66b965a452766db053f9
ddalben <ddalben@beatrix.co>
2021-06-10 20:31:29 -0300

    Initial Commit

---
+++ CSharp.Data.Structures.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace CSharp.Data.Structures.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
